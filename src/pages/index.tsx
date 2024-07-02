cc110551f800d7abd787bf6ecbf5757c9f217349
KikatoIV <39209669+KikatoIV@users.noreply.github.com>
2024-07-02 16:26:54 +0200

    Add some local storage for basket items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ProductItem } from "../types/productItem";
 import ProductGrid from "../components/ProductGrid";
 import Cart from "../components/Cart";
@@ -32,27 +31,44 @@ const Home: React.FC = () => {
     fetchProducts();
   }, []);
 
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, []);
+
+  const saveCartToLocalStorage = (updatedCart: ProductItem[]) => {
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  };
+
   const addToCart = (product: ProductItem) => {
     setCart(prevCart => {
       const existingItem = prevCart.find((item) => item.id === product.id);
       if (existingItem) {
-        return prevCart.map((item) =>
+        const updatedCart = prevCart.map((item) =>
           item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
         );
+        saveCartToLocalStorage(updatedCart);
+        return updatedCart;
       } else {
-        return [...prevCart, { ...product, quantity: 1 }];
+        const updatedCart = [...prevCart, { ...product, quantity: 1 }];
+        saveCartToLocalStorage(updatedCart);
+        return updatedCart;
       }
     });
   };
 
   const removeFromCart = (product: ProductItem) => {
-    setCart((prevCart) =>
-      prevCart
+    setCart((prevCart) => {
+      const updatedCart = prevCart
         .map((item) =>
           item.id === product.id ? { ...item, quantity: item.quantity - 1 } : item
         )
-        .filter((item) => item.quantity > 0)
-    );
+        .filter((item) => item.quantity > 0);
+      saveCartToLocalStorage(updatedCart);
+      return updatedCart;
+    });
   };
 
   return (
