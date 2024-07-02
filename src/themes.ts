08a4861e155247fac4cce8664862a7349eeaffcc
KikatoIV <39209669+KikatoIV@users.noreply.github.com>
2024-07-02 13:03:37 +0200

    Updated some styles

--- src/themes.ts
+++ src/themes.ts
@@ -1,11 +1,13 @@
-export const lightTheme = {
+import styled from 'styled-components';
+
+export const lightTheme = styled.div `
   body: "#ffffff",
   text: "#333333",
   background: "#f0f0f0",
-};
+`;
 
-export const darkTheme = {
-  body: "#333333",
-  text: "#ffffff",
-  background: "#272727",
-};
+export const darkTheme = styled.div `
+  body: "#ffffff",
+  text: "#333333",
+  background: "#f0f0f0",
+`;
